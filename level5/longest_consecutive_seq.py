1862e201bf7fc68e445ca0aadad38243b5f4513d
sanket-qp <sanket-qp@users.noreply.github.com>
2018-05-08 19:07:19 -0700

    Update longest_consecutive_seq.py

--- level5/longest_consecutive_seq.py
+++ level5/longest_consecutive_seq.py
@@ -1,3 +1,10 @@
+"""
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+Example: 
+Given [100, 4, 200, 1, 3, 2],
+The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+"""
 class Solution:
     # @param A : tuple of integers
     # @return an integer
